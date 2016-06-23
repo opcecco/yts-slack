96766e90208696f8e4333241ca04e577177af327
Oliver Ceccopieri <oliverbiotech@gmail.com>
2016-06-22 22:42:08 -0400

    He downloads

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python
 
-import json
-from urllib import urlencode
-from urllib2 import Request, urlopen
 from time import sleep
 #from slackclient import SlackClient
 
+from ytsbot import YTSBot
 import config
 
 
-# client = SlackClient(config.slack_token)
+# Hijack it here for testing
 
-# if !client.rtm_connect():
-	# print 'Connection Failed'
-	# exit()
-	
-# awaiting_choice = {}
+bot = YTSBot()
+while True:
+	userid = 'opcecco'
+	message = raw_input().lower()
+	response = bot.respond(userid, message)
+	if response is not None:
+		print response
 
-# while True:
-	# try:
-		# stream = client.rtm_read()
+exit()
+
+
+# Here's the real code
+
+client = SlackClient(config.slack_token)
+
+if not client.rtm_connect():
+	print 'Connection Failed'
+	exit()
+
+while True:
+	try:
+		stream = client.rtm_read()
 		
-		# for event in stream:
+		for event in stream:
 			
-			# if 'type' in event and 'channel' in event and 'text' in event and 'user' in event and event['type'] == 'message':
+			if 'type' in event and 'channel' in event and 'text' in event and 'user' in event and event['type'] == 'message':
 				
-				# uchannel = event['channel']
-				# text = event['text'].encode('utf8', 'ignore')
-				# userid = event['user'].encode('utf8', 'ignore')
+				uchannel = event['channel']
+				text = event['text'].encode('utf8', 'ignore')
+				userid = event['user'].encode('utf8', 'ignore')
 				
-				# response = bot.respond(userid, text)
+				response = bot.respond(userid, text)
 				
-				# if response is not None:
-					# client.rtm_send_message(uchannel, response)
+				if response is not None:
+					client.rtm_send_message(uchannel, response)
 				
-	# except:
-		# pass
+	except:
+		pass
 	
-	# sleep(config.listen_delay)
-	
-	
-def query_movie(search_term):
-	search_values = config.search_values
-	search_values['query_term'] = search_term
-	data = urlencode(search_values)
-	url = 'https://yts.ag/api/v2/list_movies.json?' + data
-	request = Request(url, headers = {'User-Agent': config.browser_spoof})
-	response = urlopen(request)
-	return json.loads(response.read())
-	
+	sleep(config.listen_delay)
---
+++ ytsbot.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import json, re, os
+from urllib import urlencode
+from urllib2 import Request, urlopen
+from random import choice
+
+import config
+
+
+class YTSBot:
+
+	def __init__(self):
+		self.awaiting_choice = {}
+
+
+	def respond(self, userid, message):
+		if 'find ' in message:
+			title = message.split('find ', 1)[1]
+			return self.find_movie(userid, title)
+		if userid in self.awaiting_choice.keys():
+			match = re.search('\d+', message)
+			if match is not None:
+				if 'about' in message:
+					return str(self.awaiting_choice[userid])
+				else:
+					return self.select_movie(userid, int(match.group()) - 1)
+		return None
+
+
+	def select_movie(self, userid, selection):
+		if selection >= len(self.awaiting_choice[userid]):
+			return 'That movie is not on the list.'
+		
+		movie = self.awaiting_choice[userid][selection]
+		del(self.awaiting_choice[userid])
+
+		url = [torrent['url'] for torrent in movie['torrents'] if torrent['quality'] == config.search_values['quality']][0]
+		self.download(url)
+		return choice(config.friendly_responses) + userid + '. I\'ll download ' + movie['title'] + ' (' + str(movie['year']) + ')!'
+
+
+	def find_movie(self, userid, movie_title):
+		data = self.query(movie_title)['data']
+
+		if data['movie_count'] == 0:
+			return 'I couldn\'t find that movie, <@' + userid + '>.'
+
+		self.awaiting_choice[userid] = data['movies']
+		outstring = 'Which movie did you mean, <@' + userid + '>?\n'
+
+		outstring += '\n'.join(['[' + str(data['movies'].index(movie) + 1) + '] ' + movie['title'] + ' (' + str(movie['year']) + ')' for movie in data['movies']])
+		return outstring
+
+
+	def download(self, url):
+		filename = url.split('/')[-1]
+		request = Request(url, headers = {'User-Agent': config.browser_spoof})
+		infile = urlopen(request)
+		
+		with open(os.path.expanduser(config.download_folder + '/' + filename), 'wb') as outfile:
+			outfile.write(infile.read())
+		return
+
+
+	def query(self, search_term):
+		search_values = config.search_values
+		search_values['query_term'] = search_term
+
+		data = urlencode(search_values)
+		url = 'https://yts.ag/api/v2/list_movies.json?' + data
+
+		request = Request(url, headers = {'User-Agent': config.browser_spoof})
+		result = urlopen(request)
+		return json.loads(result.read())
